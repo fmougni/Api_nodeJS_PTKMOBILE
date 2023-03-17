61454cf7f94c462177b3633e419a993fe67644d4
fmougni <85298590+fmougni@users.noreply.github.com>
2023-03-17 01:15:39 +0100

    update

--- test-db.js
+++ test-db.js
@@ -1,5 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
+const db = new sqlite3.Database('db_test.db');
 
 db.serialize(() => {
   // Création de la table "produits"
--- tests/test.js
+++ tests/test.js
@@ -3,51 +3,58 @@ const request = require('supertest');
 const app = require('../app');
 const db = require('../test-db');
 
+
+function createTables(done) {
+  db.serialize(() => {
+    db.run(`DROP TABLE IF EXISTS produits`);
+    db.run(`CREATE TABLE produits (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      nom TEXT,
+      description TEXT,
+      prix REAL
+    )`);
+
+    db.run(`DROP TABLE IF EXISTS medias`);
+    db.run(`CREATE TABLE medias (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      produit_id INTEGER,
+      type TEXT,
+      url TEXT,
+      FOREIGN KEY (produit_id) REFERENCES produits(id)
+    )`);
+
+    db.run(`DROP TABLE IF EXISTS utilisateurs`);
+    db.run(`CREATE TABLE utilisateurs (
+      email TEXT PRIMARY KEY,
+      mot_de_passe TEXT,
+      token TEXT
+    )`);
+
+    db.run(`DROP TABLE IF EXISTS stock`);
+    db.run(`CREATE TABLE stock (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      produit_id INTEGER,
+      quantite INTEGER,
+      FOREIGN KEY (produit_id) REFERENCES produits(id)
+    )`, done);
+  });
+}
+
+beforeEach(done => {
+  createTables(done);
+});
+
+afterEach(done => {
+  db.serialize(() => {
+    db.run(`DROP TABLE IF EXISTS produits`);
+    db.run(`DROP TABLE IF EXISTS medias`);
+    db.run(`DROP TABLE IF EXISTS utilisateurs`);
+    db.run(`DROP TABLE IF EXISTS stock`, done);
+  });
+});
+
+
 describe('Route "/produits"', () => {
-  beforeEach(done => {
-    db.serialize(() => {
-      db.run(`DROP TABLE IF EXISTS produits`);
-      db.run(`CREATE TABLE produits (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nom TEXT,
-        description TEXT,
-        prix REAL
-      )`);
-
-      db.run(`DROP TABLE IF EXISTS medias`);
-      db.run(`CREATE TABLE medias (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        produit_id INTEGER,
-        type TEXT,
-        url TEXT,
-        FOREIGN KEY (produit_id) REFERENCES produits(id)
-      )`);
-
-      db.run(`DROP TABLE IF EXISTS utilisateurs`);
-      db.run(`CREATE TABLE utilisateurs (
-        email TEXT PRIMARY KEY,
-        mot_de_passe TEXT,
-        token TEXT
-      )`);
-
-      db.run(`DROP TABLE IF EXISTS stock`);
-      db.run(`CREATE TABLE stock (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        produit_id INTEGER,
-        quantite INTEGER,
-        FOREIGN KEY (produit_id) REFERENCES produits(id)
-      )`, done);
-    });
-  });
-
-  afterEach(done => {
-    db.serialize(() => {
-      db.run(`DROP TABLE IF EXISTS produits`);
-      db.run(`DROP TABLE IF EXISTS medias`);
-      db.run(`DROP TABLE IF EXISTS utilisateurs`);
-      db.run(`DROP TABLE IF EXISTS stock`, done);
-    });
-  });
 
   it('should add a product', done => {
     const produit = {
@@ -87,51 +94,7 @@ describe('Route "/produits"', () => {
 });
 
 describe('Route "/inscription"', () => {
-  beforeEach(done => {
-    db.serialize(() => {
-      db.run(`DROP TABLE IF EXISTS produits`);
-      db.run(`CREATE TABLE produits (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nom TEXT,
-        description TEXT,
-        prix REAL
-      )`);
-
-      db.run(`DROP TABLE IF EXISTS medias`);
-      db.run(`CREATE TABLE medias (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        produit_id INTEGER,
-        type TEXT,
-        url TEXT,
-        FOREIGN KEY (produit_id) REFERENCES produits(id)
-      )`);
-
-      db.run(`DROP TABLE IF EXISTS utilisateurs`);
-      db.run(`CREATE TABLE utilisateurs (
-        email TEXT PRIMARY KEY,
-        mot_de_passe TEXT,
-        token TEXT
-      )`);
-
-      db.run(`DROP TABLE IF EXISTS stock`);
-      db.run(`CREATE TABLE stock (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        produit_id INTEGER,
-        quantite INTEGER,
-        FOREIGN KEY (produit_id) REFERENCES produits(id)
-      )`, done);
-    });
-  });
-
-  afterEach(done => {
-    db.serialize(() => {
-      db.run(`DROP TABLE IF EXISTS produits`);
-      db.run(`DROP TABLE IF EXISTS medias`);
-      db.run(`DROP TABLE IF EXISTS utilisateurs`);
-      db.run(`DROP TABLE IF EXISTS stock`, done);
-    });
-  });
-
+  
  /* it('should register a new user', done => {
     const user = {
       email: 'test@example.com',
@@ -189,111 +152,46 @@ describe('Route "/inscription"', () => {
 });
 */
 describe('Route "/produits"', () => {
-  beforeEach(done => {
-    db.serialize(() => {
-      db.run(`DROP TABLE IF EXISTS produits`);
-      db.run(`CREATE TABLE produits (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nom TEXT,
-        description TEXT,
-        prix REAL
-      )`);
 
-      db.run(`DROP TABLE IF EXISTS medias`);
-      db.run(`CREATE TABLE medias (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        produit_id INTEGER,
-        type TEXT,
-        url TEXT,
-        FOREIGN KEY (produit_id) REFERENCES produits(id)
-      )`);
-
-      db.run(`DROP TABLE IF EXISTS utilisateurs`);
-      db.run(`CREATE TABLE utilisateurs (
-        email TEXT PRIMARY KEY,
-        mot_de_passe TEXT,
-        token TEXT
-      )`);
-
-      db.run(`DROP TABLE IF EXISTS stock`);
-      db.run(`CREATE TABLE stock (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        produit_id INTEGER,
-        quantite INTEGER,
-        FOREIGN KEY (produit_id) REFERENCES produits(id)
-      )`, done);
+  it('should return a list of products if authenticated', done => {
+    // Ajouter un utilisateur avec un token dans la base de données
+    db.run(`INSERT INTO utilisateurs (email, mot_de_passe, token) VALUES ('test@test.com', 'password', 'e6fr6nt86ae')`, err => {
+      if (err) {
+        done(err);
+        return;
+      }
+      
+      // Faire une requête GET avec le token dans la query string
+      request(app)
+        .get('/produits?token_Authentification=e6fr6nt86ae')
+        .expect(200)
+        .end((err, res) => {
+          if (err) {
+            done(err);
+          } else {
+            assert.ok(res.body.length > 0);
+            done();
+          }
+        });
     });
   });
 
-  afterEach(done => {
-    db.serialize(() => {
-      db.run(`DROP TABLE IF EXISTS produits`);
-      db.run(`DROP TABLE IF EXISTS medias`);
-      db.run(`DROP TABLE IF EXISTS utilisateurs`);
-      db.run(`DROP TABLE IF EXISTS stock`, done);
-    });
-  });
-
-  it('should return a list of products', done => {
+  it('should return an error if not authenticated', done => {
     request(app)
       .get('/produits')
-      .expect(200)
+      .expect(401)
       .end((err, res) => {
         if (err) {
           done(err);
         } else {
-          assert.ok(res.body.length > 0);
+          assert.strictEqual(res.text, 'L\'utilisateur n\'est pas authentifié.');
           done();
         }
       });
   });
-});
+})
 
 describe('Route "/authentification"', () => {
-  beforeEach(done => {
-    db.serialize(() => {
-      db.run(`DROP TABLE IF EXISTS produits`);
-      db.run(`CREATE TABLE produits (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nom TEXT,
-        description TEXT,
-        prix REAL
-      )`);
-
-      db.run(`DROP TABLE IF EXISTS medias`);
-      db.run(`CREATE TABLE medias (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        produit_id INTEGER,
-        type TEXT,
-        url TEXT,
-        FOREIGN KEY (produit_id) REFERENCES produits(id)
-      )`);
-
-      db.run(`DROP TABLE IF EXISTS utilisateurs`);
-      db.run(`CREATE TABLE utilisateurs (
-        email TEXT PRIMARY KEY,
-        mot_de_passe TEXT,
-        token TEXT
-      )`);
-
-      db.run(`DROP TABLE IF EXISTS stock`);
-      db.run(`CREATE TABLE stock (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        produit_id INTEGER,
-        quantite INTEGER,
-        FOREIGN KEY (produit_id) REFERENCES produits(id)
-      )`, done);
-    });
-  });
-
-  afterEach(done => {
-    db.serialize(() => {
-      db.run(`DROP TABLE IF EXISTS produits`);
-      db.run(`DROP TABLE IF EXISTS medias`);
-      db.run(`DROP TABLE IF EXISTS utilisateurs`);
-      db.run(`DROP TABLE IF EXISTS stock`, done);
-    });
-  });
 
   it('should return a token', done => {
     const user = {
